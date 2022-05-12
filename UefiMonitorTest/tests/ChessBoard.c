a13b494c590bec360e03f2d4d311a707a4c4a03a
Joursoir <chat@joursoir.net>
2022-05-12 12:27:13 +0300

    add the chess board test stubs

---
+++ UefiMonitorTest/tests/ChessBoard.c
@@ -0,0 +1,43 @@
+#include "tests/ChessBoard.h"
+
+VOID
+ChessBoardTestInit (
+  IN UMT_CONTEXT *Ctx
+  )
+{
+
+}
+
+VOID
+ChessBoardTestDoit (
+  IN UMT_CONTEXT *Ctx
+  )
+{
+
+}
+
+VOID
+ChessBoardTestTip (
+  IN UMT_CONTEXT *Ctx
+  )
+{
+
+}
+
+VOID
+ChessBoardTestChangeParam (
+  IN  UMT_CONTEXT *Ctx,
+  IN  INT8        ParamStep
+  )
+{
+
+}
+
+VOID
+ChessBoardTestChangeValue (
+  IN  UMT_CONTEXT *Ctx,
+  IN  INT8        ValueStep
+  )
+{
+
+}
