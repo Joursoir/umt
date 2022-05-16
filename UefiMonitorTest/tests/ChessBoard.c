1cd97298c5634c16a2aa584106dc4560d2ac4ee9
HyperNiki <giperdrakon@gmail.com>
2022-05-16 21:44:22 +0300

    test chess board: restore code

--- UefiMonitorTest/tests/ChessBoard.c
+++ UefiMonitorTest/tests/ChessBoard.c
@@ -13,24 +13,21 @@ ChessBoardTestInit (
 {
   UINT32 I;
   UINT32 J;
-  UINT32 ColorSquare;
-  UINT32 FirstColorSquare;
+  enum UMT_COLORS ColorSquare;
+  enum UMT_COLORS FirstColorSquare;
+  enum UMT_COLORS TempColor;
   GRAPHICS_CONTEXT *Graphics = Ctx->Graphics;
 
   FirstColorSquare = UMT_COLOR_WHITE;
+  ColorSquare      = UMT_COLOR_BLACK;
 
-  for (J = 0; J < Graphics->Height; J = J + CurrentSideLength)
+  for (J = 0; J < Graphics->Height; J += CurrentSideLength)
   {
-    if (FirstColorSquare == UMT_COLOR_WHITE)
-    {
-      FirstColorSquare = UMT_COLOR_BLACK;
-      ColorSquare = UMT_COLOR_WHITE;
-    } else {
-      FirstColorSquare = UMT_COLOR_WHITE;
-      ColorSquare = UMT_COLOR_BLACK;
-    }
+    TempColor = FirstColorSquare;
+    FirstColorSquare = ColorSquare;
+    ColorSquare = TempColor;
 
-    for (I = 0; I < Graphics->Width; I = I + CurrentSideLength)
+    for (I = 0; I < Graphics->Width; I += CurrentSideLength)
     {
       PutRect (Graphics,
                I,
@@ -39,11 +36,7 @@ ChessBoardTestInit (
                J + CurrentSideLength,
                &gUmtColors[ColorSquare].Color);
 
-      if (ColorSquare == UMT_COLOR_WHITE) {
-        ColorSquare = UMT_COLOR_BLACK;
-      } else {
-        ColorSquare = UMT_COLOR_WHITE;
-      }
+      ColorSquare = ((ColorSquare == UMT_COLOR_WHITE) ? UMT_COLOR_BLACK : UMT_COLOR_WHITE);
     }
   }
 
@@ -65,7 +58,7 @@ ChessBoardTestTip (
   IN UMT_CONTEXT *Ctx
   )
 {
- GRAPHICS_CONTEXT *Graphics;
+  GRAPHICS_CONTEXT *Graphics;
 
   Graphics = Ctx->Graphics;
 
@@ -118,22 +111,25 @@ ChessBoardTestChangeValue (
 
   Height   = Ctx->Graphics->Height;
   Width    = Ctx->Graphics->Width;
-  ValueOut = CurrentSideLength;
+  ValueOut = CurrentSideLength + ValueStep;
 
-  ValueOut += ValueStep;
+  if (ValueOut == 0) {
+    return;
+  }
 
-  while   ((((Width / ValueOut) * ValueOut != Width) ||
+  while ((((Width / ValueOut) * ValueOut != Width) ||
          ((Height / ValueOut) * ValueOut != Height)) &&
          ((ValueOut < Height) && (ValueOut < Width)) &&
-         (ValueOut > 1))
+         (ValueOut > 2))
   {
     ValueOut += ValueStep;
   }
 
-    if ((ValueOut < Height) && (ValueOut < Width) && (ValueOut > 0))
-    {
-      CurrentSideLength = ValueOut;
-    }
+  if ((ValueOut >= Height) || (ValueOut >= Width))
+  {
+    return;
+  }
 
+  CurrentSideLength = ValueOut;
   ChessBoardTestInit (Ctx);
 }
